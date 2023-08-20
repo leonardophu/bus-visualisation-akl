f34a51e16b6dfa073221cd5f20ce4825e01adfbe
leonardophu <leonardophu@gmail.com>
2023-08-20 14:25:14 +1200

    setting up points object

--- NodeJS/TESTINGdatabase/public/javascript/map/startup.js
+++ NodeJS/TESTINGdatabase/public/javascript/map/startup.js
@@ -1,51 +1,40 @@
-
-let requiredData 
-let roads
-let busStatus
-let intPoints
-let startTime
-
 window.addEventListener("load", async function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const busNumber = urlParams.get('busNumber');
+  const urlParams = new URLSearchParams(window.location.search);
+  const busNumber = urlParams.get('busNumber');
 
-    requiredData = await fetchBusData(busNumber);
-    roads = await getRoad(requiredData);
+  const requiredData = await fetchBusData(busNumber);
+  const roads = await getRoad(requiredData);
 
-    busStatus, intPoints, startTime = await getBusInterpolatedData(busNumber);
-});
+  const {busStatus, intPoints, startTime} = await getBusInterpolatedData(busNumber);
 
-const num_destinations = intPoints.length;
-let minTimestamp = startTime.MAX_VALUE;
+  const num_destinations = intPoints.length;
+  let minTimestamp = startTime.MAX_VALUE;
 
-// Initialisation
-origin = [];
-points_index = [];
-points_waiting_index = [];
-points_obj = [];
-currentBusStatus = [];
+  // Initialisation
+  origin = [];
+  points_index = [];
+  points_waiting_index = [];
+  points_obj = [];
+  currentBusStatus = [];
 
-function initialise() {
-  for (let i = 0; i < num_destinations; i++) {
-    origin.push(intPoints[i][0]);
-    currentBusStatus.push(busStatus[i][0]);
-    points_obj.push(points(i));
-    points_waiting_index.push(i);
+  function initialise() {
+    for (let i = 0; i < num_destinations; i++) {
+      origin.push(intPoints[i][0]);
+      currentBusStatus.push(busStatus[i][0]);
+      points_obj.push(points(origin[i]));
+      points_waiting_index.push(i);
+    }
   }
-}
-// Get the desired points
-initialise();
+  // Get the desired points
+  initialise();
 
-// Keeps the initial points 
-var originalPointsWaitingIndex = JSON.parse(JSON.stringify(points_waiting_index));
-var originalInterpolationPoints = JSON.parse(JSON.stringify(intPoints));
-var originalBusStatus = JSON.parse(JSON.stringify(currentBusStatus));
-var originalBusStatusInterpolation = JSON.parse(JSON.stringify(busStatus));
+  // Keeps the initial points 
+  var originalPointsWaitingIndex = JSON.parse(JSON.stringify(points_waiting_index));
+  var originalInterpolationPoints = JSON.parse(JSON.stringify(intPoints));
+  var originalBusStatus = JSON.parse(JSON.stringify(currentBusStatus));
+  var originalBusStatusInterpolation = JSON.parse(JSON.stringify(busStatus));
 
-console.log(busStatus);
-
-map.on('load', () => {
+  map.on('load', () => {
     addRouteLayer(roads);
-
+  });
 });
-
