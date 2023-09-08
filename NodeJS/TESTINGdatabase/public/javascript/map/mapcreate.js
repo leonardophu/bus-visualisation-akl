8805e2a5d019f32be0f0436eef6c49d169391466
leonardophu <leonardophu@gmail.com>
2023-09-08 16:12:09 +1200

    cancellation and interpolation updates

--- NodeJS/TESTINGdatabase/public/javascript/map/mapcreate.js
+++ NodeJS/TESTINGdatabase/public/javascript/map/mapcreate.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [0, 0], // starting position [lng, lat]
+    center: [174.7645, -36.8509], // starting position [lng, lat]
     zoom: 11 // starting zoom
 });
 
