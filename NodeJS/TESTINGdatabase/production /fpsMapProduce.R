0f5b43cd2f687c90d92d0d81fc49a513a608cba6
leonardophu <leonardophu@gmail.com>
2023-09-12 15:38:49 +1200

    generated frames and created animation

--- NodeJS/TESTINGdatabase/production /fpsMapProduce.R
+++ NodeJS/TESTINGdatabase/production /fpsMapProduce.R
@@ -2,15 +2,26 @@ library(leaflet)
 library(DBI)
 library(RODBC)
 library(odbc)
+library(mapview)
 
 source('connection.R')
 source('step_generator.R')
 source('frame_generator.R')
 
-
-
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R") %>%
-  setView(lng=174.768, lat=-36.852, zoom = 10.5)
-m  # Print the map
+createMap = function(fps, seconds) {
+  # Clearing the images folder
+  files_to_remove <- list.files(path = "images/", pattern = "m*.png", full.names = TRUE)
+  if (length(files_to_remove) > 0) {
+    file.remove(files_to_remove)
+  }
+  
+  # Creating the image
+  steps = step_generator(fps, seconds)
+  for(timestamp in steps) {
+    frame_generator(timestamp)
+  }
+  
+  library(magick)
+  img <- image_read(list.files(path = "images/", pattern="m*.png", full.names = TRUE))
+  image_animate(img, loop = 1, optimize = TRUE, fps = 20)
+}
--- NodeJS/TESTINGdatabase/production /frame_generator.R
+++ NodeJS/TESTINGdatabase/production /frame_generator.R
@@ -1,10 +1,25 @@
 frame_generator = function(timestamp) {
+  colours = c('grey', 'blue', 'orange', 'red')
   
   busses = dbGetQuery(con, paste0("SELECT longitude, 
                                   latitude, 
                                   status, 
                                   route
                                   FROM points_fps WHERE timestamp = ", timestamp))
-  return(busses)
+  
+  m <- leaflet() %>%
+    addTiles() %>%
+    setView(lng=174.768, lat=-36.852, zoom = 10)
+  
+  for (i in 1 :nrow(busses)) {
+    m <- m %>% addCircleMarkers(lng = busses$longitude[i], 
+                                lat = busses$latitude[i], 
+                                radius = 3,  # adjust the radius as you like
+                                color = colours[busses$status[i] + 1],  # adjust the color as you like
+                                fillOpacity = 1) 
+  }
+  mapshot(m, file = paste0("/images/m", timestamp, ".png"), delay = 0.25) # delay to ensure all tiles are loaded
 }
+
+
 
