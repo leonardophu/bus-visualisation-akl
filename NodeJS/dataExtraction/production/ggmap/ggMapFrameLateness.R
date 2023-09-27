4fd3f47ead4c13a0f11cce114c47161048bb73e4
leonardophu <leonardophu@gmail.com>
2023-09-27 18:58:28 +1300

    commenting code

--- NodeJS/dataExtraction/production/ggmap/ggMapFrameLateness.R
+++ NodeJS/dataExtraction/production/ggmap/ggMapFrameLateness.R
@@ -5,13 +5,18 @@ getFrameLate = function(timestamp, filepath = "frames/") {
   hour <- format(posix_time, format="%H")
   minute <- format(posix_time, format="%M")
   
+  # Points for current timestamp
   timed_data = subset(intData, timestamps == timestamp)
+  # Later bus gets plotted on later. Missing buses get placed first
   timed_data = timed_data %>% arrange(status)
+  
+  # Zoomed points for current timestamp
   zoomed_points = timed_data %>% filter(lon >= bbox2[1], 
                                         lon <= bbox2[3], 
                                         lat >= bbox2[2], 
                                         lat <= bbox2[4])
   
+  # Textgrob which contains information of the time
   timeGrob = textGrob(
     label = paste0("Time: ", hour, ":", minute), 
     x = 0.99,     
@@ -20,14 +25,16 @@ getFrameLate = function(timestamp, filepath = "frames/") {
     gp = gpar(fontsize = 140, col = "black", fontfamily = "oswald")
   )
   
+  # Big map creation
   auckland_visual = ggmap(auckland_map) + 
     geom_point(data = timed_data, 
                aes(x = lon, y = lat, color = status, alpha = as.factor(status)), 
                size = 1.5) +
+    # More relevant points get less transparancy (e.g. Late buses less transparent)
     scale_alpha_manual(values = c('0' = 0.5, '1' = 0.7, '2' = 0.8, '3' = 9)) +
     scale_color_manual(values = colour_scheme) +
     
-    # Add a square
+    # Add a square for zoomed in area
     annotate("rect", 
              xmin = bbox2[1], 
              xmax = bbox2[3], 
@@ -51,8 +58,12 @@ getFrameLate = function(timestamp, filepath = "frames/") {
                                b = 0,  # Bottom margin
                                l = 0)) # Left margin
   
+  # Zoomed visual 
   zoomed_visual = ggmap(auckland_central) + 
-    geom_point(data = zoomed_points, aes(x = lon, y= lat, color = status), size = 2.5, alpha = 0.6) +
+    geom_point(data = zoomed_points, 
+               aes(x = lon, y = lat, color = status, alpha = as.factor(status)), 
+               size = 2.5) +
+    scale_alpha_manual(values = c('0' = 0.5, '1' = 0.7, '2' = 0.8, '3' = 9)) +
     scale_color_manual(values = colour_scheme) +
     theme(axis.text.x = element_blank(),
           axis.text.y = element_blank(),
@@ -62,17 +73,21 @@ getFrameLate = function(timestamp, filepath = "frames/") {
           axis.ticks.y=element_blank(),
           # Remove the legend
           legend.position = "none",
+          # Depending on border level, change margins to remove white space 
           plot.margin = margin(t = 0,  # Top margin
                                r = 0,  # Right margin
                                b = -3,  # Bottom margin
                                l = -3)) # Left margin
   
-  # In order ot use gggrid, we need to convert our zoomed_visual into a grob
+  # In order to use gggrid, convert zoomed_visual to grob
   g2 = ggplotGrob(zoomed_visual)
+  # Box for zoomed visual
   outerEdge = rectGrob(gp = gpar(fill = NA, lwd = 1.5))
 
+  # Viewport for zoomed visual
   vp <- viewport(x = 1, y = 1, width = unit(0.498, "npc"), height = unit(0.4, "npc"), just = c("right", "top"))
   
+  # Creating our final visualisation
   frame_visual = auckland_visual + grid_panel(gTree(children = gList(g2, outerEdge), vp=vp))
   frame_visual = frame_visual + grid_panel(grobTree(timeGrob)) 
   frame_visual
