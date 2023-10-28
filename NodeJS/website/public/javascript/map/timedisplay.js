c9ae8cef80a9508f4baca7c8c72c5720b16bc53f
leonardophu <leonardophu@gmail.com>
2023-10-28 16:49:20 +1300

    removing full date time

--- NodeJS/website/public/javascript/map/timedisplay.js
+++ NodeJS/website/public/javascript/map/timedisplay.js
@@ -3,23 +3,6 @@ function updateCounterValue(counter) {
     document.getElementById("timeNowDisplay").textContent = currentTime;
 }
 
-function getDate(second) {
-    const milliseconds = (second - 43200) * 1000
-    const date = new Date(milliseconds);
-    
-    //Get components of the date 
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1; // Note: Months are zero-based, so we add 1
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-
-    return(formattedDate);
-}
-
 function getHoursAndMinutes(second) {
     const milliseconds = (second - 43200) * 1000;
     const date = new Date(milliseconds);
