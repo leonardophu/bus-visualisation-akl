5a172280bb5c31f0432ca5afffffee212422d19f
leonardophu <leonardophu@gmail.com>
2023-11-02 16:00:19 +1300

    removed api key

--- NodeJS/website/public/javascript/map/mapcreate.js
+++ NodeJS/website/public/javascript/map/mapcreate.js
@@ -1,4 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibHBodTUwMCIsImEiOiJjbGhlcHNtZjAwcTdqM2xyNDNuMHBvcXg5In0.dfh6B9xx0-XC3AI6EZs8bg';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
