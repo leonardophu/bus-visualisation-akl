ca0627831ff71e76b1394f6411f563a8899592c2
leonardophu <leonardophu@gmail.com>
2023-11-03 16:58:52 +1300

    cancellation.js

--- NodeJS/website/public/javascript/map/cancellation.js
+++ NodeJS/website/public/javascript/map/cancellation.js
@@ -1,4 +1,8 @@
+// Contains function to add cancellation, input is i which is index
+
 function addCancellation(i) {
+    // Get the cancellation box object
     const directions = document.getElementById('directions');
+    // Add the cancelled bus onto the cancellation box object
     directions.innerHTML += `<li> Bus ${busNumber} cancelled at ${getHoursAndMinutes(startTime[i])} </li>`;
 }
