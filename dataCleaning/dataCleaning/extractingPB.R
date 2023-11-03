227d7fe9d1ba7c5edfed65ed70506d653a64322c
leonardophu <leonardophu@gmail.com>
2023-11-03 13:16:33 +1300

    extractingPB.R commenting

--- dataCleaning/dataCleaning/extractingPB.R
+++ dataCleaning/dataCleaning/extractingPB.R
@@ -1,10 +1,13 @@
+# Given the directory path, obtain all the files (we want vechile_locations)
 files = list.files(path = directory_path, pattern = "^vehicle_locations_.*")
 
+# Function that given a file name, will convert the pb to a dataframe
 extractData = function(file_name) {
   a = read(transit_realtime.FeedMessage, file_name)
   pb_to_df(a)
 }
 
+# Function to convert pb file to a dataframe. Extracts all the columns of interest
 pb_to_df <- function(feed) {
   ## converts protobuf GTFS file into a dataframe
   do.call(rbind, lapply(feed$entity, function(x) {
@@ -18,6 +21,8 @@ pb_to_df <- function(feed) {
                current_stop_sequence = x$vehicle$current_stop_sequence)
   }))
 }
+
+# Parallelisation 
 
 # Determine the number of cores to use (minus 1 to leave one free)
 num_cores <- detectCores() - 1
